21d9ea01a0b4913b1a904ffa5668c0da0c8e6430
tradebe <tradebe@student.wethinkcode.co.za>
2020-11-26 10:45:06 +0200

    clean master branch

--- group_project/doctor/doc.py
+++
@@ -1,6 +0,0 @@
-import mutual_assist.mutual_assist
-
-def help():
-    print("That is great. Time to pick a topic.")
-    mutual_assist.mutual_assist.topics()
-  
--- group_project/mutual_assist/mutual_assist.py
+++
@@ -1,9 +0,0 @@
-def topics():
-    available_topics = ['loops', 'strings', 'modules']
-    print(f"Here are the topics of you can choose from:\n")
-    for elem in range(0,len(available_topics)):
-        print(available_topics[elem])
-# topics()
-
-#Abigail: I think all the functions that both the doctor and patient
-# use (in terms of user input) should be put in here. 
