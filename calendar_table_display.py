aee149c9cc56b38890b842f7b77eb286c637ae24
Thandeka <59987213+tmngomez@users.noreply.github.com>
2020-11-27 10:20:38 +0200

    Update calendar_table_display.py

--- calendar_table_display.py
+++ calendar_table_display.py
@@ -7,6 +7,8 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
+'''
+#This part can be taken from the other module
 
 scopes = ['https://www.googleapis.com/auth/calendar']
 flow = InstalledAppFlow.from_client_secrets_file("client_secret.json", scopes= scopes)
@@ -15,14 +17,21 @@ pickle.dump(credentials, open("token.pkl", "wb"))
 creds = pickle.load(open("token.pkl", "rb"))
 
 service = build('calendar', 'v3', credentials=creds)
+'''
 
+
+#Getting the current date and time in isoformat, meaning that it will have the 'T' and the microseconds, as well as the timezone
 now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+
+#This will return the next 10 coming events from your calendar starting from the time of request
 print('Getting the upcoming 10 events')
 events_result = service.events().list(calendarId='primary', timeMin=now,
                                     maxResults=10, singleEvents=True,
                                     orderBy='startTime').execute()
 events = events_result.get('items', [])
+#you have to print events to see the output
 
+#This displays the next 10 events into a readable table format
 event_table = []
 event_table.append(('Date', 'Time', 'Topic/Title'))
 
